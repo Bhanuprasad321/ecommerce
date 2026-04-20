5ad67f26f313ce7033121851c248ec222a6de7d9
Bhanuprasad321 <kanugulabhanu012@gmail.com>
2026-04-20 17:32:30 +0530

    update cors and axios for deployment

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,25 +1,24 @@
 // Step 1 — import axios
-import axios from 'axios'
+import axios from "axios";
 
 // Step 2 — create instance
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api'
-})
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+});
 
 // Step 3 — add interceptor
 api.interceptors.request.use((config) => {
-
   // get token from localStorage
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem("token");
 
   // if token exists attach it to header
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   // always return config
-  return config
-})
+  return config;
+});
 
 // Step 4 — export
-export default api
+export default api;
